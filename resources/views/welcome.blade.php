3ec6359a7126f36531d5ff5b1011aa7f2ba8148b
Swornim Shrestha <srestaswrnm@www.schoophile.dev>
2016-12-01 19:39:30 +0545

    Some Clean Ups

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,15 +75,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Qube API Development Phase
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/bravegurkha/qube">View on github</a>
+                <a href="https://github.com/bravegurkha/qube/issues">Report Issue</a>
                 </div>
             </div>
         </div>
